0d088508f88e5803dfeb02a086e1e77864b48155
Bill Gestrich <bill@foreflight.com>
2018-03-07 08:42:05 -0500

    Update xcode project after adding Swift Package Manager

--- JiraSwiftTests/JiraSwiftTests.swift
+++ JiraSwiftTests/JiraSwiftTests.swift
@@ -21,49 +21,7 @@ class JiraSwiftTests: XCTestCase {
     }
     
     func testExample() {
-        
-        let username = getConfigurationFileVariable(key: "JIRA_USERNAME") ?? ""
-        let password = getConfigurationFileVariable(key: "JIRA_PASSWORD") ?? ""
-        let baseURL = getConfigurationFileVariable(key: "JIRA_URL") ?? ""
 
-        let jiraClient = JiraRestClient(baseURL:baseURL, auth:BasicAuth(username: username, password: password))
-        
-        
-        //        let filter = IssueFilter(assignee: "bill", status:"open")
-//        let jqlFilter = JQLFilter(jql: "assignee=bill AND status=Open")
-//                
-//        let expectation = XCTestExpectation(description: "issuesForAsnc test")
-//
-//        jiraClient.issuesFor(filter: jqlFilter) { (issues) in
-//            print(issues)
-//            expectation.fulfill()
-//
-//        }
-//        
-//        wait(for: [expectation], timeout: 100.0)
-        
-
-        
-//        let expectation = XCTestExpectation(description: "issuesForAsnc test")
-//        
-//        jiraClient.getBoards { (boards) in
-//            expectation.fulfill()
-//            print(boards)
-//        }
-//        
-//        wait(for: [expectation], timeout: 100.0)
-        
-        let expectation = XCTestExpectation(description: "issuesForAsnc test")
-        
-        jiraClient.getBoards(completionBlock: { (boards) in
-            print(boards)
-            expectation.fulfill()
-        }, errorBlock: { (error) in
-            print(error)
-            expectation.fulfill()
-        })
-        
-        wait(for: [expectation], timeout: 100.0)
 
     }
     
--- Tests/JiraSwiftTests/JiraSwiftTests.swift
+++ Tests/JiraSwiftTests/JiraSwiftTests.swift
@@ -3,10 +3,49 @@ import XCTest
 
 class JiraSwiftTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(JiraSwift().text, "Hello, World!")
+        
+        let username = getConfigurationFileVariable(key: "JIRA_USERNAME") ?? ""
+        let password = getConfigurationFileVariable(key: "JIRA_PASSWORD") ?? ""
+        let baseURL = getConfigurationFileVariable(key: "JIRA_URL") ?? ""
+        
+        let jiraClient = JiraRestClient(baseURL:baseURL, auth:BasicAuth(username: username, password: password))
+        
+        
+        //        let filter = IssueFilter(assignee: "bill", status:"open")
+        //        let jqlFilter = JQLFilter(jql: "assignee=bill AND status=Open")
+        //                
+        //        let expectation = XCTestExpectation(description: "issuesForAsnc test")
+        //
+        //        jiraClient.issuesFor(filter: jqlFilter) { (issues) in
+        //            print(issues)
+        //            expectation.fulfill()
+        //
+        //        }
+        //        
+        //        wait(for: [expectation], timeout: 100.0)
+        
+        
+        
+        //        let expectation = XCTestExpectation(description: "issuesForAsnc test")
+        //        
+        //        jiraClient.getBoards { (boards) in
+        //            expectation.fulfill()
+        //            print(boards)
+        //        }
+        //        
+        //        wait(for: [expectation], timeout: 100.0)
+        
+        let expectation = XCTestExpectation(description: "issuesForAsnc test")
+        
+        jiraClient.getBoards(completionBlock: { (boards) in
+            print(boards)
+            expectation.fulfill()
+        }, errorBlock: { (error) in
+            print(error)
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 100.0)
     }
 
 
