8fe87a4e0ad7509c4802ca08877bb68f4e08d0b2
Bill Gestrich <bill@foreflight.com>
2018-03-18 07:54:19 -0400

    Add JiraSwift-iOS framework target

---
+++ JiraSwift-iOS/JiraSwift_iOS.h
@@ -0,0 +1,18 @@
+//
+//  JiraSwift_iOS.h
+//  JiraSwift-iOS
+//
+//  Created by Bill Gestrich on 3/18/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JiraSwift_iOS.
+FOUNDATION_EXPORT double JiraSwift_iOSVersionNumber;
+
+//! Project version string for JiraSwift_iOS.
+FOUNDATION_EXPORT const unsigned char JiraSwift_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JiraSwift_iOS/PublicHeader.h>
+
+
