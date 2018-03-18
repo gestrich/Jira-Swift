773541eaf1c74df523ee3abf8ca706cdef34685a
Bill Gestrich <bill@foreflight.com>
2018-03-18 12:25:25 -0400

    Fix example

--- Tests/JiraSwiftTests/JiraSwiftTests.swift
+++ Tests/JiraSwiftTests/JiraSwiftTests.swift
@@ -30,18 +30,31 @@ class JiraSwiftTests: XCTestCase {
         let jiraClient = JiraRestClient(baseURL:baseURL, auth:BasicAuth(username: username, password: password))
         
         
-        //        let filter = IssueFilter(assignee: "bill", status:"open")
-        //        let jqlFilter = JQLFilter(jql: "assignee=bill AND status=Open")
-        //                
-        //        let expectation = XCTestExpectation(description: "issuesForAsnc test")
-        //
-        //        jiraClient.issuesFor(filter: jqlFilter) { (issues) in
-        //            print(issues)
-        //            expectation.fulfill()
-        //
-        //        }
-        //        
-        //        wait(for: [expectation], timeout: 100.0)
+        let filter = IssueFilter(assignee: "bill", status:"open")
+        let jqlFilter = JQLFilter(jql: "assignee=bill AND status=Open")
+        
+        let expectation = XCTestExpectation(description: "issuesForAsnc test")
+        
+        jiraClient.issues(for: jqlFilter, completionBlock: { (issues) in
+            for issue : Issue in issues {
+                print(issue.key)
+                print(issue.urlString)
+                print(issue.fields)
+            }
+            print(issues)
+            expectation.fulfill()
+            
+        }, errorBlock: { (error) in
+                print(error)
+                expectation.fulfill()            
+        })
+        
+        wait(for: [expectation], timeout: 100.0)
+//        jiraClient.issues(for: jqlFilter, completionBlock: { (issues) in
+
+//        }, errorB
+                
+
         
         
         
@@ -54,17 +67,17 @@ class JiraSwiftTests: XCTestCase {
         //        
         //        wait(for: [expectation], timeout: 100.0)
         
-        let expectation = XCTestExpectation(description: "issuesForAsnc test")
-        
-        jiraClient.getBoards(completionBlock: { (boards) in
-            print(boards)
-            expectation.fulfill()
-        }, errorBlock: { (error) in
-            print(error)
-            expectation.fulfill()
-        })
-        
-        wait(for: [expectation], timeout: 100.0)
+//        let expectation = XCTestExpectation(description: "issuesForAsnc test")
+//        
+//        jiraClient.getBoards(completionBlock: { (boards) in
+//            print(boards)
+//            expectation.fulfill()
+//        }, errorBlock: { (error) in
+//            print(error)
+//            expectation.fulfill()
+//        })
+//        
+//        wait(for: [expectation], timeout: 100.0)
     }
 
 
