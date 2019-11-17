eeb893fcaf4d81fca65d628a97c009b14c243739
Bill Gestrich <bill@foreflight.com>
2019-11-17 12:25:08 -0500

    Update

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/gestrich/swift-utilities.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JiraSwift",
-            dependencies: []),
+            dependencies: ["swift-utilities"]),
         .testTarget(
             name: "JiraSwiftTests",
             dependencies: ["JiraSwift"]),
